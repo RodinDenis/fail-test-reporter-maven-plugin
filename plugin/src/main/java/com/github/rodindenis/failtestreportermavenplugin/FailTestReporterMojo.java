5535f9baf22673437fd21442032895570a756d29
RodinDenis <denis.for.logs@gmail.com>
2021-02-23 00:21:48 +0300

    added reprots parameter to the mojo

--- plugin/src/main/java/com/github/rodindenis/failtestreportermavenplugin/FailTestReporterMojo.java
+++ plugin/src/main/java/com/github/rodindenis/failtestreportermavenplugin/FailTestReporterMojo.java
@@ -5,11 +5,16 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "fail-test-reporter", defaultPhase = LifecyclePhase.TEST)
 public class FailTestReporterMojo extends AbstractMojo {
+
+    @Parameter(property = "reports")
+    String reports;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Hello from Mojo");
+        getLog().info(String.format("reports parameter is %s", reports));
     }
 }
