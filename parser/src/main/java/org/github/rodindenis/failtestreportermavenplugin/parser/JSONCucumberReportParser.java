b014bfbc9b8088ab3014e60d59bd46ce1a8309cb
RodinDenis <denis.for.logs@gmail.com>
2021-02-23 23:56:12 +0300

    added basic parser logic

--- parser/src/main/java/org/github/rodindenis/failtestreportermavenplugin/parser/JSONCucumberReportParser.java
+++ parser/src/main/java/org/github/rodindenis/failtestreportermavenplugin/parser/JSONCucumberReportParser.java
@@ -7,13 +7,26 @@ import org.github.rodindenis.failtestreportermavenplugin.parser.features.JSONFea
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JSONCucumberReportParser {
 
     private ObjectMapper mapper = new ObjectMapper();
+    private List<JSONFeature> features;
 
-    public List<JSONFeature> parse(File file) throws IOException {
-        return mapper.readValue(file, new TypeReference<List<JSONFeature>>() {
+    public JSONCucumberReportParser(File file) throws IOException {
+        features = mapper.readValue(file, new TypeReference<List<JSONFeature>>() {
         });
     }
+
+    public int getFailedFeaturesCount() {
+        List<JSONFeature> failedFeatures = features.stream()
+                .filter(feature -> feature.getElements().stream()
+                        .flatMap(element -> element.getSteps().stream())
+                        .anyMatch(step -> step.getResult().getStatus() != "passed")
+                ).collect(Collectors.toList());
+
+        return failedFeatures.size();
+    }
+
 }
--- plugin/src/main/java/com/github/rodindenis/failtestreportermavenplugin/plugin/FailTestReporterMojo.java
+++ plugin/src/main/java/com/github/rodindenis/failtestreportermavenplugin/plugin/FailTestReporterMojo.java
@@ -7,11 +7,9 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.github.rodindenis.failtestreportermavenplugin.parser.JSONCucumberReportParser;
-import org.github.rodindenis.failtestreportermavenplugin.parser.features.JSONFeature;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 @Mojo(name = "fail-test-reporter", defaultPhase = LifecyclePhase.POST_INTEGRATION_TEST)
 public class FailTestReporterMojo extends AbstractMojo {
@@ -23,12 +21,15 @@ public class FailTestReporterMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().debug(String.format("reports parameter is %s", report));
 
-        JSONCucumberReportParser parser = new JSONCucumberReportParser();
+        JSONCucumberReportParser parser;
 
         try {
-            List<JSONFeature> features = parser.parse(new File(report));
+            parser = new JSONCucumberReportParser(new File(report));
         } catch (IOException e) {
             throw new MojoFailureException(String.format("Can't read report file %s", report), e);
         }
+
+        getLog().info("Failed features count: " + parser.getFailedFeaturesCount());
+
     }
 }
