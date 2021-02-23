9958e1adf09de7f5dbd1003d1be62020294caf9c
RodinDenis <denis.for.logs@gmail.com>
2021-02-24 00:14:23 +0300

    stream for count changed to for

--- parser/src/main/java/org/github/rodindenis/failtestreportermavenplugin/parser/JSONCucumberReportParser.java
+++ parser/src/main/java/org/github/rodindenis/failtestreportermavenplugin/parser/JSONCucumberReportParser.java
@@ -3,11 +3,12 @@ package org.github.rodindenis.failtestreportermavenplugin.parser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.github.rodindenis.failtestreportermavenplugin.parser.features.JSONFeature;
+import org.github.rodindenis.failtestreportermavenplugin.parser.features.scenarios.JSONScenario;
+import org.github.rodindenis.failtestreportermavenplugin.parser.features.scenarios.steps.JSONStep;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class JSONCucumberReportParser {
 
@@ -20,13 +21,26 @@ public class JSONCucumberReportParser {
     }
 
     public int getFailedFeaturesCount() {
-        List<JSONFeature> failedFeatures = features.stream()
-                .filter(feature -> feature.getElements().stream()
-                        .flatMap(element -> element.getSteps().stream())
-                        .anyMatch(step -> step.getResult().getStatus() != "passed")
-                ).collect(Collectors.toList());
+        int count = 0;
+        for (JSONFeature feature : features) {
+            for (JSONScenario element : feature.getElements()) {
+                for (JSONStep step : element.getSteps()) {
+                    if (step.getResult().getStatus() != "passed") {
+                        ++count;
+                    }
+                }
+            }
+        }
+        return count;
 
-        return failedFeatures.size();
+        //too many memory for big files ((
+//        List<JSONFeature> failedFeatures = features.stream()
+//                .filter(feature -> feature.getElements().stream()
+//                        .flatMap(element -> element.getSteps().stream())
+//                        .anyMatch(step -> step.getResult().getStatus() != "passed")
+//                ).collect(Collectors.toList());
+//
+//        return failedFeatures.size();
     }
 
 }
